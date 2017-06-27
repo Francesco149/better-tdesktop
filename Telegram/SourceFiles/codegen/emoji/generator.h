83304919f5d9aaa3dee8ec4d93c484ba8d342ee7
Franc[e]sco <lolisamurai@tfwno.gf>
2017-06-27 05:17:42 +0200

    codegen_emoji: remove unused include

--- Telegram/SourceFiles/codegen/emoji/generator.h
+++ Telegram/SourceFiles/codegen/emoji/generator.h
@@ -22,7 +22,6 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 
 #include <memory>
 #include <QtCore/QString>
-#include <QtCore/QSet>
 #include "codegen/common/cpp_file.h"
 #include "codegen/emoji/options.h"
 #include "codegen/emoji/data.h"
